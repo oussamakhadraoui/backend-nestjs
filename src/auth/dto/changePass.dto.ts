262fc508fc8346a852121ba688072a2b4c1653f8
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-08-15 20:43:20 +0100

    change password functionality

--- src/auth/dto/changePass.dto.ts
+++ src/auth/dto/changePass.dto.ts
@@ -1,7 +1,13 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class changePassDto {
+  @MinLength(8)
   @IsNotEmpty()
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*W+)(?![./n])(?=.*[A-Z])(?=.*[a-z])).*$/, {
+    message:
+      'password is too weak it must contain at least 1 spacial character 1 uppercase character and 1 number!',
+  })
   newPass: string;
   @IsNotEmpty()
   confirmNewPass: string;
